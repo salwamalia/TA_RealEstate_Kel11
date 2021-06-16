82086ac5c82e1f06a749dabde02aeffacf456609
Aurelia Agatha <0320200019@polman.astra.ac.id>
2021-06-16 14:32:44 +0700

    dgBeli

---
+++ TA_RealEstate_Kel11/TransaksiPembelian.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace TA_RealEstate_Kel11
+{
+    partial class TransaksiPembelian
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransaksiPembelian));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.bunifuTileButton1 = new Bunifu.Framework.UI.BunifuTileButton();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuGradientPanel2 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.btnLogout = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnPenyewaan = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnPembelian = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.dgPembelian = new Bunifu.Framework.UI.BunifuCustomDataGrid();
+            this.bunifuGradientPanel1.SuspendLayout();
+            this.bunifuGradientPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPembelian)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.bunifuTileButton1);
+            this.bunifuGradientPanel1.Controls.Add(this.bunifuCustomLabel1);
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.DarkMagenta;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.LightSkyBlue;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.Transparent;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.Transparent;
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(1264, 122);
+            this.bunifuGradientPanel1.TabIndex = 2;
+            // 
+            // bunifuTileButton1
+            // 
+            this.bunifuTileButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuTileButton1.color = System.Drawing.Color.Transparent;
+            this.bunifuTileButton1.colorActive = System.Drawing.Color.Transparent;
+            this.bunifuTileButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuTileButton1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.bunifuTileButton1.Font = new System.Drawing.Font("Century Gothic", 15.75F);
+            this.bunifuTileButton1.ForeColor = System.Drawing.Color.White;
+            this.bunifuTileButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuTileButton1.Image")));
+            this.bunifuTileButton1.ImagePosition = 20;
+            this.bunifuTileButton1.ImageZoom = 50;
+            this.bunifuTileButton1.LabelPosition = 41;
+            this.bunifuTileButton1.LabelText = "Kasir";
+            this.bunifuTileButton1.Location = new System.Drawing.Point(1146, 0);
+            this.bunifuTileButton1.Margin = new System.Windows.Forms.Padding(6);
+            this.bunifuTileButton1.Name = "bunifuTileButton1";
+            this.bunifuTileButton1.Size = new System.Drawing.Size(118, 122);
+            this.bunifuTileButton1.TabIndex = 1;
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bunifuCustomLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Blackadder ITC", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(3, 9);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(180, 99);
+            this.bunifuCustomLabel1.TabIndex = 0;
+            this.bunifuCustomLabel1.Text = "Real Estate";
+            this.bunifuCustomLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // bunifuGradientPanel2
+            // 
+            this.bunifuGradientPanel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel2.BackgroundImage")));
+            this.bunifuGradientPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel2.Controls.Add(this.btnLogout);
+            this.bunifuGradientPanel2.Controls.Add(this.btnPenyewaan);
+            this.bunifuGradientPanel2.Controls.Add(this.btnPembelian);
+            this.bunifuGradientPanel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bunifuGradientPanel2.GradientBottomLeft = System.Drawing.Color.SkyBlue;
+            this.bunifuGradientPanel2.GradientBottomRight = System.Drawing.Color.Plum;
+            this.bunifuGradientPanel2.GradientTopLeft = System.Drawing.Color.White;
+            this.bunifuGradientPanel2.GradientTopRight = System.Drawing.Color.White;
+            this.bunifuGradientPanel2.Location = new System.Drawing.Point(0, 122);
+            this.bunifuGradientPanel2.Name = "bunifuGradientPanel2";
+            this.bunifuGradientPanel2.Quality = 10;
+            this.bunifuGradientPanel2.Size = new System.Drawing.Size(188, 532);
+            this.bunifuGradientPanel2.TabIndex = 3;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Activecolor = System.Drawing.Color.Transparent;
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLogout.BorderRadius = 0;
+            this.btnLogout.ButtonText = "Logout";
+            this.btnLogout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogout.DisabledColor = System.Drawing.Color.Transparent;
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogout.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnLogout.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnLogout.Iconimage")));
+            this.btnLogout.Iconimage_right = null;
+            this.btnLogout.Iconimage_right_Selected = null;
+            this.btnLogout.Iconimage_Selected = null;
+            this.btnLogout.IconMarginLeft = 0;
+            this.btnLogout.IconMarginRight = 0;
+            this.btnLogout.IconRightVisible = true;
+            this.btnLogout.IconRightZoom = 0D;
+            this.btnLogout.IconVisible = true;
+            this.btnLogout.IconZoom = 90D;
+            this.btnLogout.IsTab = false;
+            this.btnLogout.Location = new System.Drawing.Point(0, 98);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnLogout.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnLogout.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnLogout.selected = false;
+            this.btnLogout.Size = new System.Drawing.Size(188, 49);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Textcolor = System.Drawing.Color.White;
+            this.btnLogout.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnPenyewaan
+            // 
+            this.btnPenyewaan.Activecolor = System.Drawing.Color.Transparent;
+            this.btnPenyewaan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnPenyewaan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPenyewaan.BorderRadius = 0;
+            this.btnPenyewaan.ButtonText = "Penyewaan";
+            this.btnPenyewaan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPenyewaan.DisabledColor = System.Drawing.Color.Transparent;
+            this.btnPenyewaan.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPenyewaan.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnPenyewaan.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnPenyewaan.Iconimage")));
+            this.btnPenyewaan.Iconimage_right = null;
+            this.btnPenyewaan.Iconimage_right_Selected = null;
+            this.btnPenyewaan.Iconimage_Selected = null;
+            this.btnPenyewaan.IconMarginLeft = 0;
+            this.btnPenyewaan.IconMarginRight = 0;
+            this.btnPenyewaan.IconRightVisible = true;
+            this.btnPenyewaan.IconRightZoom = 0D;
+            this.btnPenyewaan.IconVisible = true;
+            this.btnPenyewaan.IconZoom = 90D;
+            this.btnPenyewaan.IsTab = false;
+            this.btnPenyewaan.Location = new System.Drawing.Point(0, 49);
+            this.btnPenyewaan.Name = "btnPenyewaan";
+            this.btnPenyewaan.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnPenyewaan.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnPenyewaan.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnPenyewaan.selected = false;
+            this.btnPenyewaan.Size = new System.Drawing.Size(188, 49);
+            this.btnPenyewaan.TabIndex = 2;
+            this.btnPenyewaan.Text = "Penyewaan";
+            this.btnPenyewaan.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPenyewaan.Textcolor = System.Drawing.Color.White;
+            this.btnPenyewaan.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnPembelian
+            // 
+            this.btnPembelian.Activecolor = System.Drawing.Color.Transparent;
+            this.btnPembelian.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnPembelian.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPembelian.BorderRadius = 0;
+            this.btnPembelian.ButtonText = "Pembelian";
+            this.btnPembelian.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPembelian.DisabledColor = System.Drawing.Color.Transparent;
+            this.btnPembelian.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPembelian.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnPembelian.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnPembelian.Iconimage")));
+            this.btnPembelian.Iconimage_right = null;
+            this.btnPembelian.Iconimage_right_Selected = null;
+            this.btnPembelian.Iconimage_Selected = null;
+            this.btnPembelian.IconMarginLeft = 0;
+            this.btnPembelian.IconMarginRight = 0;
+            this.btnPembelian.IconRightVisible = true;
+            this.btnPembelian.IconRightZoom = 0D;
+            this.btnPembelian.IconVisible = true;
+            this.btnPembelian.IconZoom = 90D;
+            this.btnPembelian.IsTab = false;
+            this.btnPembelian.Location = new System.Drawing.Point(0, 0);
+            this.btnPembelian.Name = "btnPembelian";
+            this.btnPembelian.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnPembelian.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnPembelian.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnPembelian.selected = false;
+            this.btnPembelian.Size = new System.Drawing.Size(188, 49);
+            this.btnPembelian.TabIndex = 1;
+            this.btnPembelian.Text = "Pembelian";
+            this.btnPembelian.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPembelian.Textcolor = System.Drawing.Color.White;
+            this.btnPembelian.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuCustomLabel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(188, 122);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(1076, 49);
+            this.bunifuCustomLabel2.TabIndex = 136;
+            this.bunifuCustomLabel2.Text = "Pembelian";
+            this.bunifuCustomLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgPembelian
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgPembelian.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgPembelian.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dgPembelian.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgPembelian.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgPembelian.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgPembelian.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPembelian.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgPembelian.DoubleBuffered = true;
+            this.dgPembelian.EnableHeadersVisualStyles = false;
+            this.dgPembelian.HeaderBgColor = System.Drawing.Color.SeaGreen;
+            this.dgPembelian.HeaderForeColor = System.Drawing.Color.SeaGreen;
+            this.dgPembelian.Location = new System.Drawing.Point(188, 482);
+            this.dgPembelian.Name = "dgPembelian";
+            this.dgPembelian.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgPembelian.Size = new System.Drawing.Size(1076, 172);
+            this.dgPembelian.TabIndex = 137;
+            // 
+            // TransaksiPembelian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 654);
+            this.Controls.Add(this.dgPembelian);
+            this.Controls.Add(this.bunifuCustomLabel2);
+            this.Controls.Add(this.bunifuGradientPanel2);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.Name = "TransaksiPembelian";
+            this.Text = "FormPembelian";
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.bunifuGradientPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPembelian)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private Bunifu.Framework.UI.BunifuTileButton bunifuTileButton1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel2;
+        private Bunifu.Framework.UI.BunifuFlatButton btnLogout;
+        private Bunifu.Framework.UI.BunifuFlatButton btnPenyewaan;
+        private Bunifu.Framework.UI.BunifuFlatButton btnPembelian;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomDataGrid dgPembelian;
+    }
+}
